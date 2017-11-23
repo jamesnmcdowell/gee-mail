3874475f7e635d622feea669901f26ac2e098e31
Arch <Arch@Archs-MacBook-Pro.local>
2017-11-23 14:04:06 -0500

    add functionality of opening email and take data with it for whatever email is clicked. Have backbutton to take you back to inbox. Style email view.

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
       <div class="header2">
         <button @click="refresh"> click me </button>
         <button @click="pushMail"> push me </button>
+        <button @click="resetSelectedMail"> back </button>
       </div>
     </header>
 
@@ -15,14 +16,20 @@
     <SideBar class = "sidebar" :inboxCount="mail.length"    />
     <article class="main">
     <div class="mail-container">
-    <div v-if="selectedMail !== undefined">
-      <h1> sup bitch </h1>
-
+    <div  v-if="selectedMail !== undefined" class="mail-view" v-bind:class="{ active: isActive }">
+      <h3> {{selectedMail.subject}}  </h3>
+      <div class="align-lr">
+        <div class="sender-info">
+        <img src="//ssl.gstatic.com/ui/v1/icons/mail/profile_mask_2x.png" style="background-color: #cccccc">
+        <p class="sender"> {{selectedMail.sender}} </p>
+        </div>
+        <p class="date"> {{selectedMail.date.toDateString()}} </p>
+      </div>
+      <p class="body"> {{selectedMail.body}} </p>
     </div>
     <div v-else>
-      <MailRow @click="openEmail(data)" v-for="data in mail" key="data.time" :rowData="data" />
+      <MailRow @click.native="openEmail(data)" v-for="data in mail" key="data.time" :rowData="data" />
     </div>
-
     </div>
     </article>
   </div>
@@ -50,6 +57,7 @@ export default {
     return {
       mail: genMail,
       selectedMail: undefined,
+      isActive: true,
     }
   },
   methods: {
@@ -62,17 +70,25 @@ export default {
     openEmail: function (data) {
       console.log("hi");
       this.selectedMail = data;
+    },
+    resetSelectedMail: function (data) {
+      this.selectedMail = undefined;
     }
   }
 }
 </script>
 
 <style lang="scss">
+
+
+
+
+
 html, body{
   margin: 0;
   padding: 0;
   width: 100%;
-  height: 100%;
+  // height: 100%;
   overflow-x: hidden;
 }
 header{
@@ -115,6 +131,52 @@ header{
 
 .mail-container {
   margin-right: 2em;
+
+  .active {
+    height: 100vh;
+    background-color: #FFFFFF;
+
+  h3 {
+    border-bottom: 1px solid #A8A8A8;
+    padding: .5em .6em;
+    font-weight: 400;
+    margin-bottom: 0;
+  }
+  .align-lr {
+    justify-content: space-between;
+    display: flex;
+    padding: 0 .7em;
+    margin-top: .5em;
+    .sender-info{
+      display: flex;
+      img {
+        width: 30px;
+        height: 30px;
+        margin-right: 10px;
+      }
+
+    p {
+      margin: 0;
+      &.sender {
+        font-weight: 600;
+      }
+    }
+
+}
+.date {
+  margin-right: 40px;
+  margin-bottom: 0;
+  margin-top:  0;
+}
+  }
+  .body {
+    padding: 0 3.2em;
+    border-bottom: 1px solid #A8A8A8;
+    padding-bottom: 3em;
+  }
+
+  }
+
 }
 #app {
   background-image: url('https://brushbucktours.com/wp-content/uploads/2015/01/mormon-row.jpg') ;
